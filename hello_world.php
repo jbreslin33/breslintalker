e3883424c739d8ba86627b57d4445ebcc4d00a02
jbreslin33 <jbreslin33@localhost>
2011-05-11 19:14:35 -0400

    cleanup

--- hello_world.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-
-<?php
-echo "Hello World, fuck it i'll do it live";
-?>
-
-</body>
-</html>
--- variables.php
+++
@@ -1,8 +0,0 @@
-<?
-$GLOBALS['auth_host']  = "localhost:3306";
-$GLOBALS['auth_user']  = "jbreslin";
-$GLOBALS['auth_pass']  = "mibesfat";
-$GLOBALS['auth_dbase'] = "roacheopen";
-
-?>
-
