462c713b2dcdc7fde1b8c277ec08463696251c54
Jim Breslin <jbreslin33@gmail.com>
2016-07-21 12:36:32 -0400

    fix

--- ranks.php
+++ ranks.php
@@ -773,140 +773,6 @@ for ($t = 1; $t < $teams + 1; $t++)
 
   <br><br><br>
 *********************************************************
-		<h1 class="title">Player Ranks, Unable to Attend: <br> </h1>
-
-
-
-<br>
-
-
-
-
-
-
-
-<br>
-
-   <!-- Set up the table -->
-
-
-  <table border='1'>
-
-   <tr>
-
-
-
-   </tr>
-
-   <!-- Retrieve records from database -->
-   <?php
-
-   $query = "select *";
-   $query .= " from roacheopen.golfers where status = 3 ";
-   $query .= " order by currentrank;";
-   $dbResult = pg_query($query);
-
-   if (!$dbResult) {
-     die("Database error...");
-   }
-
-   $num = pg_num_rows($dbResult);
-
-   if ($num >= 0 && $num < 5)
-     $teams = 1;
-   if ($num >= 5 && $num < 9)
-     $teams = 2;
-   if ($num >= 9 && $num < 13)
-     $teams = 3;
-   if ($num >= 13 && $num < 17)
-     $teams = 4;
-   if ($num >= 17 && $num < 21)
-     $teams = 5;
-   if ($num >= 21 && $num < 25)
-     $teams = 6;
-   if ($num >= 25 && $num < 29)
-     $teams = 7;
-   if ($num >= 29 && $num < 33)
-     $teams = 8;
-   if ($num >= 33 && $num < 37)
-     $teams = 9;
-   if ($num >= 37 && $num < 41)
-     $teams = 10;
-   if ($num >= 41 && $num < 45)
-     $teams = 11;
-   if ($num >= 45 && $num < 49)
-     $teams = 12;
-   if ($num >= 49 && $num < 53)
-     $teams = 13;
-   if ($num >= 53 && $num < 57)
-     $teams = 14;
-   if ($num >= 57 && $num < 61)
-     $teams = 15;
-   if ($num >= 61 && $num < 65)
-     $teams = 16;
-   if ($num >= 65 && $num < 69)
-     $teams = 17;
-   if ($num >= 69 && $num < 73)
-     $teams = 18;
-   if ($num >= 73 && $num < 77)
-     $teams = 19;
-   if ($num >= 77 && $num < 81)
-     $teams = 20;
-
-
-
-
-
-   if ($num == 0) {
-     echo '<tr><td colspan="4">';
-     echo 'Database Query Retrieved Nothing!</td></tr>';
-   }
-   $i = 0;
-        echo
-        "<tr>
-                         <td> <b> ***     </b>  </td>
-                         <td> <b> ********************  Captains ************  </b>  </td>
-                         </tr>\n";
-
-   while ($i < $num) {
-     $realname      = pg_Result ($dbResult, $i, 'realname');
-     $curRank       = $i + 1;
-     echo
-"<tr>
-                         <td> <b> $curRank         </b>  </td>
-                         <td> <b> $realname        </b>  </td>
-
-                         </tr>\n";
-
-     if ($curRank == $teams) {
-        echo
-        "<tr>
-                         <td> <b> ***     </b>  </td>
-                         <td> <b> ********************  1st Rounders ************  </b>  </td>
-                         </tr>\n";
-     }
-     if ($curRank == $teams * 2) {
-        echo
-        "<tr>
-                         <td> <b> ***     </b>  </td>
-                         <td> <b> ********************  2nd Rounders ************  </b>  </td>
-                         </tr>\n";
-     }
-     if ($curRank == $teams * 3) {
-        echo
-        "<tr>
-                         <td> <b> ***     </b>  </td>
-                         <td> <b> ********************  3rd Rounders ************  </b>  </td>
-                         </tr>\n";
-     }
-
-
-     $i++;
-   }
-   ?>
-
-   <!-- Close out the table and end -->
-   </table>
    <br><br><br>
 Detailed Scouting Report for Unable to Attend:
    <!-- Set up the table -->
@@ -996,6 +862,7 @@ Detailed Scouting Report for Unable to Attend:
 
      $i++;
    }
+pg_close($db);
    ?>
 
    <!-- Close out the table and end -->
